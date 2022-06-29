5822003cf058cb10ba1a44085392b55a544bbd8d
Andrés Ayelo Reig <71845375+Diecitrece@users.noreply.github.com>
2022-06-30 00:26:18 +0200

    Revert "Backend get one"

--- backend/src/infrastructure/shared/database/initDB.ts
+++
@@ -1,9 +0,0 @@
-import reflectMetadata from 'reflect-metadata';
-import { DataSource } from 'typeorm';
-
-export const initDB = (dataSource: DataSource) => {
-  dataSource
-    .initialize()
-    .then(async () => console.log('Database up and ready!'))
-    .catch((error) => console.log(`Database connection error -> ${error}`));
-};
