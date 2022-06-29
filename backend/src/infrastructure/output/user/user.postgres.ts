9d66a43416a5bc44a37afcc21bb871ee183f7c07
Andrés Ayelo Reig <diecitrece026@gmail.com>
2022-06-29 23:32:57 +0200

    feat: create createUsers endpoint

--- backend/src/infrastructure/output/user/user.postgres.ts
+++ backend/src/infrastructure/output/user/user.postgres.ts
@@ -4,9 +4,16 @@ import UserModel from '@shared/database/user/user.model';
 import { UserRepository } from '@ports/output/user.repository.port';
 
 export const userRepositoryPostgres = (): UserRepository => {
+  const userRepository = postgresDatasource.getRepository(UserModel);
+  const create = async (name: string): Promise<User | undefined> => {
+    const exists = await userRepository.findOne({ where: { name: name } });
+    if (exists) return undefined;
+    const user: User = await userRepository.save({ name });
+    return user;
+  };
   const getAll = async (): Promise<User[]> => {
-    const users: User[] = await postgresDatasource.manager.find(UserModel);
+    const users: User[] = await userRepository.find();
     return users;
   };
-  return { getAll };
+  return { getAll, create };
 };
