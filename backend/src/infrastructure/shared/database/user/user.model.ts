ccab5597406f3d5693438656588b45ffa93ff7e1
Andrés Ayelo Reig <diecitrece026@gmail.com>
2022-06-30 13:41:14 +0200

    feat: create manyToMany table for connections between users

--- backend/src/infrastructure/shared/database/user/user.model.ts
+++ backend/src/infrastructure/shared/database/user/user.model.ts
@@ -1,17 +1,29 @@
 import { EntitySchema } from 'typeorm';
 import { User } from '@domain/user.model';
 
-const UserModel = new EntitySchema<User>({
+interface ConnectedUser extends User {
+  connections: User[];
+}
+const UserModel = new EntitySchema<ConnectedUser>({
   name: 'User',
+  tableName: 'users',
   columns: {
     id: {
       primary: true,
       generated: 'uuid',
-      type: String,
+      type: 'uuid',
     },
     name: {
       type: String,
       length: 30,
+    },
+  },
+  relations: {
+    connections: {
+      target: 'users',
+      type: 'many-to-many',
+      joinTable: true,
+      cascade: true,
     },
   },
 });
